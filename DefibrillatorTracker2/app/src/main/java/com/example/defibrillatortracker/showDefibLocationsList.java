b47382176ba7d99c428d1e648f86bcd2a6932918
Oussoubicodes <130454123+Oussoubicodes@users.noreply.github.com>
2023-04-15 13:37:38 +0100

    Add files via upload

---
+++ DefibrillatorTracker2/app/src/main/java/com/example/defibrillatortracker/showDefibLocationsList.java
@@ -0,0 +1,14 @@
+package com.example.defibrillatortracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showDefibLocationsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_defib_locations_list);
+    }
+}
